df93ff6a8577d35bfe5ec395e218febfd861039f
amir-shz <amirsharif2079@gmail.com>
2023-12-20 18:02:31 +0330

    get forecast

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = "17e6283a0811f0c5f90b06d47cc5dc4b";
+const DAYS = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 const searchInput = document.querySelector("input");
 const searchButton = document.querySelector("button");
 const weatherContainer = document.getElementById("weather");
 const locationIcon = document.getElementById("location");
+const forecastContainer = document.getElementById("forecast");
 
 const getCurrentWeatherByName = async (city) => {
   const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
@@ -15,6 +25,20 @@ const getCurrentWeatherByName = async (city) => {
 
 const getCurrentWeatherByCoordinates = async (lat, lon) => {
   const url = `${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const response = await fetch(url);
+  const result = await response.json();
+  return result;
+};
+
+const getForecastWeatherByCoordinates = async (lat, lon) => {
+  const url = `${BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const response = await fetch(url);
+  const result = await response.json();
+  return result;
+};
+
+const getForecastWeatherByName = async (city) => {
+  const url = `${BASE_URL}/forecast?q=${city}&appid=${API_KEY}&units=metric`;
   const response = await fetch(url);
   const result = await response.json();
   return result;
@@ -39,8 +63,31 @@ const renderCurrentWeather = (data) => {
   weatherContainer.innerHTML = weatherJSX;
 };
 
+const getWeekDay = (date) => {
+  return DAYS[new Date(date * 1000).getDay()];
+};
+
+const renderForecastWeather = (data) => {
+  forecastContainer.innerHTML = "";
+  data = data.list.filter((obj) => obj.dt_txt.endsWith("12:00:00"));
+  data.forEach((item) => {
+    const forecastJSX = `
+      <div>
+        <img src="https://openweathermap.org/img/w/${
+          item.weather[0].icon
+        }.png" alt="weather icon"/>
+        <h3>${getWeekDay(item.dt)}</h3>
+        <p>${Math.round(item.main.temp)} °C</p>
+        <span>${item.weather[0].main}</span>
+      </div>
+    `;
+    forecastContainer.innerHTML += forecastJSX;
+  });
+};
+
 const searchHandler = async () => {
   const cityName = searchInput.value;
+  searchInput.value = "";
 
   if (!cityName) {
     alert("please enter city name");
@@ -48,12 +95,19 @@ const searchHandler = async () => {
 
   const currentData = await getCurrentWeatherByName(cityName);
   renderCurrentWeather(currentData);
+  const forecastData = await getForecastWeatherByName(cityName);
+  renderForecastWeather(forecastData);
 };
 
 const positionCallback = async (position) => {
   const { latitude, longitude } = position.coords;
   const currentData = await getCurrentWeatherByCoordinates(latitude, longitude);
   renderCurrentWeather(currentData);
+  const forecastData = await getForecastWeatherByCoordinates(
+    latitude,
+    longitude
+  );
+  renderForecastWeather(forecastData);
 };
 
 const errorCallback = (error) => {
@@ -70,3 +124,11 @@ const locationHandler = () => {
 
 searchButton.addEventListener("click", searchHandler);
 locationIcon.addEventListener("click", locationHandler);
+
+////////////////
+
+window.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    searchHandler();
+  }
+});
